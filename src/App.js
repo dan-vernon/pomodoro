be5b986aed4c3fe56ad9eaa7553098bd449e9854
Dan <danvernon@gmail.com>
2019-05-07 17:13:25 +0100

    react hooks to control app state

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import './App.css';
 import { Col, Container, Row } from 'react-bootstrap';
 
@@ -9,18 +9,41 @@ import ControlButtons from './components/control_buttons';
 import Timer from './components/timer';
 
 function App() {
+  const [breakLength, setBreakLength] = useState(300)
+  const [sessionLength, setSessionLength] = useState(1500)
+  const [timerRunning, startTimer] = useState(false)
+  const defaultTime = 1500
+
+  useEffect(() => {
+    if (breakLength >= 3600) setBreakLength(3600)
+    if (breakLength <= 0) setBreakLength(0)
+  }, [breakLength])
+
+  useEffect(() => {
+    if (sessionLength >= 3600) setSessionLength(3600)
+    if (sessionLength <= 0) setSessionLength(0)
+  }, [sessionLength])
+
   return(
     <Container >
       <Row>
-        <Period name="break" length={5} />
-        <TimeButtons period="break" />
+        <Period name="break" length={breakLength} />
+        <TimeButtons
+          period="break"
+          handleIncrement={() => setBreakLength(breakLength + 60)}
+          handleDecrement={() => setBreakLength(breakLength - 60)}
+          />
       </Row>
       <Row>
-        <Period name="session" length={25} />
-        <TimeButtons period="session" />
+        <Period name="session" length={sessionLength} />
+        <TimeButtons
+          period="session"
+          handleIncrement={() => setSessionLength(sessionLength + 60)}
+          handleDecrement={() => setSessionLength(sessionLength - 60)}
+            />
       </Row>
       <Row>
-        <Timer timeLeft={2500} />
+        <Timer timeLeft={defaultTime} running={timerRunning} />
       </Row>
       <Row>
         <ControlButtons />
