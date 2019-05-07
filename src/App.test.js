b64e737a0f04a87016dc2f90eac2253c0542a29c
Dan <danvernon@gmail.com>
2019-05-07 10:03:25 +0100

    click events testable

--- src/App.test.js
+++ src/App.test.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-import { render } from 'react-testing-library';
+import { render, fireEvent } from 'react-testing-library';
 
 import App from './App';
 
