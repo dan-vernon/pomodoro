34daa66175af8e1dc58f3fec4c6a5b2dff891d92
Dan <danvernon@gmail.com>
2019-05-07 13:23:33 +0100

    consistent calc/display of time

--- src/components/period.js
+++ src/components/period.js
@@ -1,13 +1,15 @@
 import React from 'react';
 
 export default (props) => {
+  const minutes = Math.floor(props.length / 60)
+  const seconds = props.length % 60 < 10 ? `0${props.length % 60}` : props.length  % 60
   return (
     <div>
       <div data-testid={`${props.name}-label`}>
         {props.name} length
       </div>
       <div data-testid={`${props.name}-length`}>
-        {props.length}
+        {minutes}:{seconds}
       </div>
     </div>
   )
