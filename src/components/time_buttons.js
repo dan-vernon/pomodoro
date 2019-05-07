b8841d0a32d9c91d008a10530be22f1626e71853
Dan <danvernon@gmail.com>
2019-05-07 17:14:44 +0100

    onclick function inherited from props

--- src/components/time_buttons.js
+++ src/components/time_buttons.js
@@ -4,12 +4,18 @@ import { Button } from 'react-bootstrap';
 export default (props) => {
   return (
     <div className="buttons-container time-buttons">
-      <Button variant="success" data-testid={`${props.period}-increment`}>
-      +
-      </Button>
-      <Button variant="danger" data-testid={`${props.period}-decrement`}>
-      -
-      </Button>
-    </div>
+      <Button
+      variant="success"
+      data-testid={`${props.period}-increment`}
+      onClick={props.handleIncrement}
+      value="+"
+      />
+      <Button
+      variant="danger"
+      data-testid={`${props.period}-decrement`}
+      onClick={props.handleDecrement}
+      value="-"
+      />
+      </div>
   )
 }
