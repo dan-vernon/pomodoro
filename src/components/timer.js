3790db98bab20e21402ede5d5463b4f45f72cae2
Dan <danvernon@gmail.com>
2019-05-08 15:18:37 +0100

    removed commented out state logic code

--- src/components/timer.js
+++ src/components/timer.js
@@ -2,27 +2,13 @@ import React from 'react';
 import { Card } from 'react-bootstrap';
 
 class Timer extends React.Component {
-  // constructor(props) {
-  // super(props);
-  // this.state = {
-  //   timeLeft: this.props.timeLeft,
-  //   running: this.props.running,
-  // };
-  // }
-
+  constructor(props) {
+        super()
+      }
   render() {
     const minutes = Math.floor(this.props.timeLeft / 60)
     const seconds = this.props.timeLeft % 60 < 10 ? `0${this.props.timeLeft % 60}` : this.props.timeLeft  % 60
-    // const minutes = Math.floor(this.state.timeLeft / 60)
-    // const seconds = this.state.timeLeft % 60 < 10 ? `0${this.state.timeLeft % 60}` : this.state.timeLeft  % 60
     return (
-      <div>
-        <div id="timer-label" data-testid="timer-label">
-          {this.props.breaktime ? "Breaktime!" : "Time remaining"}
-        </div>
-        <div data-testid="time-left">
-          {minutes}:{seconds}
-        </div>
       <div className="timer p-3">
         <Card className="text-center">
           <Card.Header>
