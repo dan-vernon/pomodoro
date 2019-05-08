47fb0e61a0afd48bb72e81817a3d9cbf5b72fa69
Dan <danvernon@gmail.com>
2019-05-08 09:38:55 +0100

    start/stop functionality

--- src/components/control_buttons.js
+++ src/components/control_buttons.js
@@ -4,7 +4,7 @@ import { Button } from 'react-bootstrap';
 export default (props) => {
   return (
     <div className="buttons-container">
-      <Button variant="primary" data-testid="start_stop">
+      <Button variant="primary" data-testid="start_stop" onClick={props.handleStartStop}>
       Start/Stop
       </Button>
       <Button variant="secondary" data-testid="reset">
