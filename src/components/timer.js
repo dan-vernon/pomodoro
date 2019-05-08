1e6d3ac8f417f280705527b3f94c234448b5278d
Dan <danvernon@gmail.com>
2019-05-08 09:39:34 +0100

    convert timer state to props

--- src/components/timer.js
+++ src/components/timer.js
@@ -1,16 +1,19 @@
 import React from 'react';
 
 class Timer extends React.Component {
-  constructor(props) {
-  super(props);
-  this.state = {
-    timeLeft: this.props.timeLeft,
-    running: this.props.running,
-  };
-  }
+  // constructor(props) {
+  // super(props);
+  // this.state = {
+  //   timeLeft: this.props.timeLeft,
+  //   running: this.props.running,
+  // };
+  // }
+
   render() {
-    const minutes = Math.floor(this.state.timeLeft / 60)
-    const seconds = this.state.timeLeft % 60 < 10 ? `0${this.state.timeLeft % 60}` : this.state.timeLeft  % 60
+    const minutes = Math.floor(this.props.timeLeft / 60)
+    const seconds = this.props.timeLeft % 60 < 10 ? `0${this.props.timeLeft % 60}` : this.props.timeLeft  % 60
+    // const minutes = Math.floor(this.state.timeLeft / 60)
+    // const seconds = this.state.timeLeft % 60 < 10 ? `0${this.state.timeLeft % 60}` : this.state.timeLeft  % 60
     return (
       <div>
         <div data-testid="timer-label">
