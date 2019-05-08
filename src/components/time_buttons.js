7abb1f7d4723dfd91d2813aae29662442579556a
Dan <danvernon@gmail.com>
2019-05-08 15:15:40 +0100

    grouped duration buttons

--- src/components/time_buttons.js
+++ src/components/time_buttons.js
@@ -1,25 +1,27 @@
 import React from 'react';
-import { Button } from 'react-bootstrap';
+import { Button, ButtonGroup } from 'react-bootstrap';
 
 export default (props) => {
   return (
     <div className="buttons-container time-buttons">
-      <Button
-      variant="success"
-      data-testid={`${props.period}-increment`}
-      onClick={props.handleIncrement}
-      value="+"
-      >
-      +
-      </Button>
-      <Button
-      variant="danger"
-      data-testid={`${props.period}-decrement`}
-      onClick={props.handleDecrement}
-      value="-"
-      >
-      -
-      </Button>
-      </div>
+      <ButtonGroup>
+        <Button
+        variant="success"
+        data-testid={`${props.period}-increment`}
+        onClick={props.handleIncrement}
+        value="+"
+        >
+        +
+        </Button>
+        <Button
+        variant="danger"
+        data-testid={`${props.period}-decrement`}
+        onClick={props.handleDecrement}
+        value="-"
+        >
+        -
+        </Button>
+      </ButtonGroup>
+    </div>
   )
 }
